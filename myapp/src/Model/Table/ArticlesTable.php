1e2cd4a971c1d9c295b98471d8b2880371b70574
nero-15 <ti7805@gmail.com>
2021-03-08 21:53:42 +0900

    add

--- myapp/src/Model/Table/ArticlesTable.php
+++ myapp/src/Model/Table/ArticlesTable.php
@@ -3,10 +3,22 @@
 namespace App\Model\Table;
 
 use Cake\ORM\Table;
+use Cake\Utility\Text;
+use Cake\Event\EventInterface;
 
 class ArticlesTable extends Table
 {
-public function initialize(array $config) : void
+
+	public function beforeSave(EventInterface $event, $entity, $options)
+	{
+		if ($entity->isNew() && !$entity->slug) {
+			$sluggedTitle = Text::slug($entity->title);
+			// スラグをスキーマで定義されている最大長に調整
+			$entity->slug = substr($sluggedTitle, 0, 191);
+		}
+	}
+
+	public function initialize(array $config) : void
 	{
 		$this->addBehavior('Timestamp');
 	}
