9e416ed1855a8e461e4298099b14eda165a6129a
nero-15 <ti7805@gmail.com>
2021-03-09 21:35:46 +0900

    setting indent

--- myapp/src/Application.php
+++ myapp/src/Application.php
@@ -10,8 +10,8 @@ declare(strict_types=1);
  * Redistributions of files must retain the above copyright notice.
  *
  * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link      https://cakephp.org CakePHP(tm) Project
- * @since     3.3.0
+ * @link	  https://cakephp.org CakePHP(tm) Project
+ * @since	 3.3.0
  * @license   https://opensource.org/licenses/mit-license.php MIT License
  */
 namespace App;
@@ -44,137 +44,137 @@ use Psr\Http\Message\ServerRequestInterface;
  */
 class Application extends BaseApplication implements AuthenticationServiceProviderInterface
 {
-    /**
-     * Load all the application configuration and bootstrap logic.
-     *
-     * @return void
-     */
-    public function bootstrap(): void
-    {
-        // Call parent to load bootstrap from files.
-        parent::bootstrap();
+	/**
+	 * Load all the application configuration and bootstrap logic.
+	 *
+	 * @return void
+	 */
+	public function bootstrap(): void
+	{
+		// Call parent to load bootstrap from files.
+		parent::bootstrap();
 
-        if (PHP_SAPI === 'cli') {
-            $this->bootstrapCli();
-        } else {
-            FactoryLocator::add(
-                'Table',
-                (new TableLocator())->allowFallbackClass(false)
-            );
-        }
+		if (PHP_SAPI === 'cli') {
+			$this->bootstrapCli();
+		} else {
+			FactoryLocator::add(
+				'Table',
+				(new TableLocator())->allowFallbackClass(false)
+			);
+		}
 
-        /*
-         * Only try to load DebugKit in development mode
-         * Debug Kit should not be installed on a production system
-         */
-        if (Configure::read('debug')) {
-            $this->addPlugin('DebugKit');
-        }
+		/*
+		 * Only try to load DebugKit in development mode
+		 * Debug Kit should not be installed on a production system
+		 */
+		if (Configure::read('debug')) {
+			$this->addPlugin('DebugKit');
+		}
 
-        // Load more plugins here
-    }
+		// Load more plugins here
+	}
 
-    /**
-     * Setup the middleware queue your application will use.
-     *
-     * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
-     * @return \Cake\Http\MiddlewareQueue The updated middleware queue.
-     */
-    public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
-    {
-        $middlewareQueue
-            // Catch any exceptions in the lower layers,
-            // and make an error page/response
-            ->add(new ErrorHandlerMiddleware(Configure::read('Error')))
+	/**
+	 * Setup the middleware queue your application will use.
+	 *
+	 * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
+	 * @return \Cake\Http\MiddlewareQueue The updated middleware queue.
+	 */
+	public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
+	{
+		$middlewareQueue
+			// Catch any exceptions in the lower layers,
+			// and make an error page/response
+			->add(new ErrorHandlerMiddleware(Configure::read('Error')))
 
-            // Handle plugin/theme assets like CakePHP normally does.
-            ->add(new AssetMiddleware([
-                'cacheTime' => Configure::read('Asset.cacheTime'),
-            ]))
+			// Handle plugin/theme assets like CakePHP normally does.
+			->add(new AssetMiddleware([
+				'cacheTime' => Configure::read('Asset.cacheTime'),
+			]))
 
-            // Add routing middleware.
-            // If you have a large number of routes connected, turning on routes
-            // caching in production could improve performance. For that when
-            // creating the middleware instance specify the cache config name by
-            // using it's second constructor argument:
-            // `new RoutingMiddleware($this, '_cake_routes_')`
-            ->add(new RoutingMiddleware($this))
+			// Add routing middleware.
+			// If you have a large number of routes connected, turning on routes
+			// caching in production could improve performance. For that when
+			// creating the middleware instance specify the cache config name by
+			// using it's second constructor argument:
+			// `new RoutingMiddleware($this, '_cake_routes_')`
+			->add(new RoutingMiddleware($this))
 
-            // Parse various types of encoded request bodies so that they are
-            // available as array through $request->getData()
-            // https://book.cakephp.org/4/en/controllers/middleware.html#body-parser-middleware
-            ->add(new BodyParserMiddleware())
+			// Parse various types of encoded request bodies so that they are
+			// available as array through $request->getData()
+			// https://book.cakephp.org/4/en/controllers/middleware.html#body-parser-middleware
+			->add(new BodyParserMiddleware())
 
-            // Cross Site Request Forgery (CSRF) Protection Middleware
-            // https://book.cakephp.org/4/en/controllers/middleware.html#cross-site-request-forgery-csrf-middleware
-            ->add(new CsrfProtectionMiddleware([
-                'httponly' => true,
-            ]))
+			// Cross Site Request Forgery (CSRF) Protection Middleware
+			// https://book.cakephp.org/4/en/controllers/middleware.html#cross-site-request-forgery-csrf-middleware
+			->add(new CsrfProtectionMiddleware([
+				'httponly' => true,
+			]))
 
-            ->add(new RoutingMiddleware($this))
-            ->add(new AuthenticationMiddleware($this));
+			->add(new RoutingMiddleware($this))
+			->add(new AuthenticationMiddleware($this));
 
-        return $middlewareQueue;
-    }
+		return $middlewareQueue;
+	}
 
-    /**
-     * Register application container services.
-     *
-     * @param \Cake\Core\ContainerInterface $container The Container to update.
-     * @return void
-     * @link https://book.cakephp.org/4/en/development/dependency-injection.html#dependency-injection
-     */
-    public function services(ContainerInterface $container): void
-    {
-    }
+	/**
+	 * Register application container services.
+	 *
+	 * @param \Cake\Core\ContainerInterface $container The Container to update.
+	 * @return void
+	 * @link https://book.cakephp.org/4/en/development/dependency-injection.html#dependency-injection
+	 */
+	public function services(ContainerInterface $container): void
+	{
+	}
 
-    /**
-     * Bootstrapping for CLI application.
-     *
-     * That is when running commands.
-     *
-     * @return void
-     */
-    protected function bootstrapCli(): void
-    {
-        try {
-            $this->addPlugin('Bake');
-        } catch (MissingPluginException $e) {
-            // Do not halt if the plugin is missing
-        }
+	/**
+	 * Bootstrapping for CLI application.
+	 *
+	 * That is when running commands.
+	 *
+	 * @return void
+	 */
+	protected function bootstrapCli(): void
+	{
+		try {
+			$this->addPlugin('Bake');
+		} catch (MissingPluginException $e) {
+			// Do not halt if the plugin is missing
+		}
 
-        $this->addPlugin('Migrations');
+		$this->addPlugin('Migrations');
 
-        // Load more plugins here
-    }
+		// Load more plugins here
+	}
 
 
-    public function getAuthenticationService(ServerRequestInterface $request): AuthenticationServiceInterface
-    {
-        $authenticationService = new AuthenticationService([
-            'unauthenticatedRedirect' => '/users/login',
-            'queryParam' => 'redirect',
-        ]);
+	public function getAuthenticationService(ServerRequestInterface $request): AuthenticationServiceInterface
+	{
+		$authenticationService = new AuthenticationService([
+			'unauthenticatedRedirect' => '/users/login',
+			'queryParam' => 'redirect',
+		]);
 
-        // identifiers を読み込み、email と password のフィールドを確認します
-        $authenticationService->loadIdentifier('Authentication.Password', [
-            'fields' => [
-                'username' => 'email',
-                'password' => 'password',
-            ]
-        ]);
+		// identifiers を読み込み、email と password のフィールドを確認します
+		$authenticationService->loadIdentifier('Authentication.Password', [
+			'fields' => [
+				'username' => 'email',
+				'password' => 'password',
+			]
+		]);
 
-        //  authenticatorsをロードしたら, 最初にセッションが必要です
-        $authenticationService->loadAuthenticator('Authentication.Session');
-        // 入力した email と password をチェックする為のフォームデータを設定します
-        $authenticationService->loadAuthenticator('Authentication.Form', [
-            'fields' => [
-                'username' => 'email',
-                'password' => 'password',
-            ],
-            'loginUrl' => '/users/login',
-        ]);
+		//  authenticatorsをロードしたら, 最初にセッションが必要です
+		$authenticationService->loadAuthenticator('Authentication.Session');
+		// 入力した email と password をチェックする為のフォームデータを設定します
+		$authenticationService->loadAuthenticator('Authentication.Form', [
+			'fields' => [
+				'username' => 'email',
+				'password' => 'password',
+			],
+			'loginUrl' => '/users/login',
+		]);
 
-        return $authenticationService;
-    }
+		return $authenticationService;
+	}
 }
