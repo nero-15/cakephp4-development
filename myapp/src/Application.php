eeccfb0dd8066701e873c8d2e0b8ea517f03f22c
nero-15 <ti7805@gmail.com>
2021-03-11 23:01:59 +0900

    application indent

--- myapp/src/Application.php
+++ myapp/src/Application.php
@@ -74,44 +74,18 @@ class Application extends BaseApplication implements AuthenticationServiceProvid
 		// Load more plugins here
 	}
 
-	/**
-	 * Setup the middleware queue your application will use.
-	 *
-	 * @param \Cake\Http\MiddlewareQueue $middlewareQueue The middleware queue to setup.
-	 * @return \Cake\Http\MiddlewareQueue The updated middleware queue.
-	 */
 	public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
 	{
 		$middlewareQueue
-			// Catch any exceptions in the lower layers,
-			// and make an error page/response
 			->add(new ErrorHandlerMiddleware(Configure::read('Error')))
-
-			// Handle plugin/theme assets like CakePHP normally does.
 			->add(new AssetMiddleware([
 				'cacheTime' => Configure::read('Asset.cacheTime'),
 			]))
-
-			// Add routing middleware.
-			// If you have a large number of routes connected, turning on routes
-			// caching in production could improve performance. For that when
-			// creating the middleware instance specify the cache config name by
-			// using it's second constructor argument:
-			// `new RoutingMiddleware($this, '_cake_routes_')`
 			->add(new RoutingMiddleware($this))
-
-			// Parse various types of encoded request bodies so that they are
-			// available as array through $request->getData()
-			// https://book.cakephp.org/4/en/controllers/middleware.html#body-parser-middleware
 			->add(new BodyParserMiddleware())
-
-			// Cross Site Request Forgery (CSRF) Protection Middleware
-			// https://book.cakephp.org/4/en/controllers/middleware.html#cross-site-request-forgery-csrf-middleware
 			->add(new CsrfProtectionMiddleware([
 				'httponly' => true,
 			]))
-
-			->add(new RoutingMiddleware($this))
 			->add(new AuthenticationMiddleware($this));
 
 		return $middlewareQueue;
